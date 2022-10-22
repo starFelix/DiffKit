14987ac2eb72f07c3269c487dbf0f89f803b5b89
StarFelix <z.f.star9@gmail.com>
2022-10-22 14:45:01 +0800

    add common header file

---
+++ DiffKit/Classes/DiffKit.h
@@ -0,0 +1,22 @@
+//
+//  DiffKit.h
+//  Pods
+//
+//  Created by Star Zhu on 2022/10/22.
+//
+
+#ifndef DiffKit_h
+#define DiffKit_h
+
+#import "Algorithm.h"
+#import "ArraySection.h"
+#import "ChangeSet.h"
+#import "ContentEquatable.h"
+#import "ContentIdentifiable.h"
+#import "Differentiable.h"
+#import "DifferentiableSection.h"
+#import "ElementPath.h"
+#import "StagedChangeset.h"
+#import "UIKitExtension.h"
+
+#endif /* DiffKit_h */
